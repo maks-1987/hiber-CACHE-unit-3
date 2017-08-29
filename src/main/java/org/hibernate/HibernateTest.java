e9990c3de659ed6710d03c9ad09042bb1465373f
maks-1987 <max.moderation@gmail.com>
2017-08-29 15:52:55 +0300

    1

--- src/main/java/org/hibernate/HibernateTest.java
+++ src/main/java/org/hibernate/HibernateTest.java
@@ -7,25 +7,10 @@ import java.util.Date;
 
 public class HibernateTest {
     public static void main(String[] args) {
-        //final
-        Vehicle vehicle = new Vehicle();
-        vehicle.setVehicleName("Car");
-
-        TwoWheeler bike = new TwoWheeler();
-        bike.setVehicleName("Bike");
-        bike.setSteeringHandle("Bike Steering Handle");
-
-        FourWheeler car = new FourWheeler();
-        car.setVehicleName("Porsche");
-        car.setSteeringWheel("Porsche Steering Wheel");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
-
         session.beginTransaction();
-        session.save(vehicle);
-        session.save(bike);
-        session.save(car);
 
         session.getTransaction().commit();
         session.close();
--- src/main/java/org/hibernate/dto/UserDetails.java
+++ src/main/java/org/hibernate/dto/UserDetails.java
@@ -1,31 +1,14 @@
 package org.hibernate.dto;
 
-import org.hibernate.annotations.CollectionId;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
-
 
 public class UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
     private String userName;
-
-    @ManyToMany(cascade=CascadeType.ALL)
-    private Collection<Vehicle> vehicle = new ArrayList<Vehicle>();
-
-    public Collection<Vehicle> getVehicle() {
-        return vehicle;
-    }
-    public void setVehicle(Collection<Vehicle> vehicle) {
-        this.vehicle = vehicle;
-    }
 
     public int getUserId() {
         return userId;
@@ -33,7 +16,7 @@ public class UserDetails {
     public void setUserId(int userId) {
         this.userId = userId;
     }
-//    @Column (name = "USER_NAME")
+
     public String getUserName() {
         return userName;
     }
