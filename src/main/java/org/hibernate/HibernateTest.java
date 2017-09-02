781171d3406d7785c22f5665c0519d6bb7b38448
maks-1987 <max.moderation@gmail.com>
2017-09-02 13:36:45 +0300

    unit 2.1 Exception in thread "main" org.hibernate.hql.internal.ast.QuerySyntaxException: unexpected token: userId near line 1, column 41 [from org.hibernate.dto.UserDetails were userId > 5]

--- src/main/java/org/hibernate/HibernateTest.java
+++ src/main/java/org/hibernate/HibernateTest.java
@@ -1,34 +1,27 @@
 package org.hibernate;
 import org.hibernate.dto.*;
 
+import java.util.List;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.dto.UserDetails;
-import java.util.Date;
 
 public class HibernateTest {
     public static void main(String[] args) {
-        //UserDetails user = new UserDetails();
-        //user.setUserName("Test_User");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-
-        UserDetails user = (UserDetails) session.get(UserDetails.class, 1);
-        //session.save(user);
-        //user.setUserName("Updated_User");
-        //user.setUserName("Updated_User_Again");
+        /*for (int i=0; i<10; i++){
+            UserDetails user = new UserDetails();
+            user.setUserName("User " + i);
+            session.save(user);
+        }*/
+        Query query = session.createQuery("from UserDetails were userId > 5");
+        List users = query.list();
         session.getTransaction().commit();
         session.close();
-
-        user.setUserName("Change name");
-
-        session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.update(user);
-        //user.setUserName("Change After Update");
-        session.getTransaction().commit();
-        session.close();
+        System.out.println("Size of list result = " + users.size());
 
     }
 }
--- src/main/java/org/hibernate/dto/UserDetails.java
+++ src/main/java/org/hibernate/dto/UserDetails.java
@@ -1,10 +1,14 @@
 package org.hibernate.dto;
 
-import javax.persistence.*;
+//import javax.persistence.*;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@org.hibernate.annotations.Entity(selectBeforeUpdate = true)
+@org.hibernate.annotations.Entity(selectBeforeUpdate=true)
 public class UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
